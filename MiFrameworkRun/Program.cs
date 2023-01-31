e8a1aaa0b128a590244b962db294944ccb9f8e4f
Kyatto <1196855439@qq.com>
2023-01-31 09:52:55 +0800

    修改类库运行工程名称

---
+++ MiFrameworkRun/Program.cs
@@ -0,0 +1,13 @@
+﻿using MiFramework.Stream;
+
+ByteArray byteArray1 = new ByteArray();
+ByteArray byteArray2 = new ByteArray();
+
+byteArray1.WriteIntAdaptive(8192);
+byteArray1.WriteFloat(-0.01f);
+byteArray1.WriteString("测试字符占用量");
+
+byteArray1.WriteTo(byteArray2);
+Console.WriteLine(byteArray2.ReadIntAdaptive());
+Console.WriteLine(byteArray2.ReadFloat());
+Console.WriteLine(byteArray2.ReadString());
