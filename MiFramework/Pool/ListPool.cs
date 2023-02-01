b1e0ca668e68423e307a7f41fce2a4ad59c5532b
Kyatto <1196855439@qq.com>
2023-02-01 17:43:19 +0800

    对象池用例-列表池

---
+++ MiFramework/Pool/ListPool.cs
@@ -0,0 +1,27 @@
+﻿namespace MiFramework.Pool
+{
+    /// <summary>
+    /// 参考UnityUI对象池<br/>
+    /// List的对象池
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class ListPool<T>
+    {
+        private static readonly ObjectPool<List<T>> listPool = new(null, Clear);
+
+        private static void Clear(List<T> list)
+        {
+            list.Clear(); 
+        }
+
+        public static List<T> Get()
+        {
+            return listPool.Get();
+        }
+
+        public static void Release(List<T>? list)
+        {
+            listPool.Release(ref list);
+        }
+    }
+}
