957210afe5b8cd0edcb1f8b6658c00a7ca5b9140
Kyatto <1196855439@qq.com>
2023-02-01 15:15:56 +0800

    参考UnityUI对象池，简化对象池代码

--- MiFramework/Pool/ObjectPool.cs
+++ MiFramework/Pool/ObjectPool.cs
@@ -1,20 +1,23 @@
 ﻿namespace MiFramework.Pool
 {
-    public class ObjectPool<T> where T : IPoolObject, new()
+    /// <summary>
+    /// 参考UnityUI对象池<br/>
+    /// 感觉像是把创建和回收时，对象要执行的清理方法或别的什么方法交给了调用方处理<br/>
+    /// 这个池针对引用类型回收做了一点手脚，回收后引用置空避免调用方仍然能继续使用该引用<br/>
+    /// 但是这样做就不能将值类型作为池内对象来存储了
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ObjectPool<T> where T : class, new()
     {
-        private uint idCounter;
-        private readonly Dictionary<uint, T> objects;
+        private readonly Action<T>? OnGetObject;
+        private readonly Action<T>? OnReleaseObject;
         private readonly Stack<T> stack;
 
-        public ObjectPool()
+        public ObjectPool(Action<T>? onSpawnObject, Action<T>? onDespawn, int capacity = 16)
         {
-            objects = new Dictionary<uint, T>();
-            stack = new Stack<T>();
-        }
+            OnGetObject = onSpawnObject;
+            OnReleaseObject = onDespawn;
 
-        public ObjectPool(int capacity)
-        {
-            objects = new Dictionary<uint, T>(capacity);
             stack = new Stack<T>(capacity);
         }
 
@@ -22,31 +25,32 @@
         {
             T element;
 
-            if (stack.Count == 0)
-            {
-                element = new T() { PoolObjectID = idCounter++ };
-                objects.Add(element.PoolObjectID, element);
-            }
-            else
+            if (stack.Count > 0)
             {
                 element = stack.Pop();
             }
-            
-            element.Clear();
-            element.IsActive = true;
+            else
+            {
+                element = new();
+            }
+
+            OnGetObject?.Invoke(element);
 
             return element;
         }
 
-        public void Release(T element)
+        public void Release(ref T? element)
         {
-            if (objects.TryGetValue(element.PoolObjectID, out var e) && e.IsActive)
-            {
-                element.Clear();
-                element.IsActive = false;
+            if (element == null || stack.Contains(element))
+                return;
 
-                stack.Push(element);
-            }
+            OnReleaseObject?.Invoke(element);
+
+            T temp = element;
+
+            stack.Push(temp);
+
+            element = null;
         }
     }
 }
