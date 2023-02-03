d714bb11af7137123328409e7073778acedcae9b
Kyatto <1196855439@qq.com>
2023-02-03 14:33:26 +0800

    [数据结构]改进为平衡二叉搜索树（AVL）

--- MiFramework/Struct/BinarySearchTree.cs
+++ MiFramework/Struct/BinarySearchTree.cs
@@ -1,10 +1,13 @@
-﻿namespace MiFramework.Struct
+﻿using System.Xml.Linq;
+
+namespace MiFramework.Struct
 {
     public class BinarySearchTree<T> where T : IComparable
     {
         private class TreeNode
         {
             public T element;
+            public int height;
             public TreeNode? leftChild;
             public TreeNode? rightChild;
 
@@ -19,51 +22,128 @@
             {
                 return element.CompareTo(otherNode.element);
             }
+
+            public void UpdateHeight()
+            {
+                int leftHeight = leftChild?.height ?? -1;
+                int rightHeight = rightChild?.height ?? -1;
+                height = Math.Max(leftHeight, rightHeight) + 1;
+            }
+
+            public int GetBalanceFactor()
+            {
+                int leftHeight = leftChild?.height ?? -1;
+                int rightHeight = rightChild?.height ?? -1;
+                return leftHeight - rightHeight;
+            }
         }
 
         private TreeNode? root;
 
-        public void Add(T element)
+        private TreeNode? RotateRight(TreeNode root)
         {
-            TreeNode newNode = new(element);
+            TreeNode? child = root.leftChild;
+            if (child == null) return root;
 
-            if (root == null)
+            TreeNode? grandChild = child.rightChild;
+
+            child.rightChild = root;
+            root.leftChild = grandChild;
+
+            root.UpdateHeight();
+            child.UpdateHeight();
+            
+            return child;
+        }
+
+        private TreeNode? RotateLeft(TreeNode root)
+        {
+            TreeNode? child = root.rightChild;
+            if (child == null) return root;
+
+            TreeNode? grandChild = child.leftChild;
+
+            child.leftChild = root;
+            root.rightChild = grandChild;
+
+            root.UpdateHeight();
+            child.UpdateHeight();
+
+            return child;
+        }
+
+        private TreeNode? Rotate(TreeNode root)
+        {
+            int rootBalanceFactor = root.GetBalanceFactor();
+            // 右侧偏高
+            if (rootBalanceFactor < -1)
             {
-                root = newNode;
-                return;
-            }
-
-            TreeNode? node = root;
-
-            while (node != null)
-            {
-                int compareResult = newNode.CompareTo(node);
-
-                if (compareResult == 0)
+                if (root.rightChild == null)
                 {
-                    return;
+                    return root;
                 }
-                else if (compareResult > 0)
+
+                int childBalanceFactor = root.rightChild.GetBalanceFactor();
+                
+                if (childBalanceFactor <= 0)
                 {
-                    if (node.rightChild == null)
-                    {
-                        node.rightChild = newNode;
-                        return;
-                    }
-                    
-                    node = node.rightChild;
+                    return RotateLeft(root);
                 }
                 else
                 {
-                    if (node.leftChild == null)
-                    {
-                        node.leftChild = newNode;
-                        return;
-                    }
-                    
-                    node = node.leftChild;
+                    root.rightChild = RotateRight(root.rightChild);
+                    return RotateLeft(root);
                 }
             }
+            else if (rootBalanceFactor > 1)
+            {
+                if (root.leftChild == null)
+                {
+                    return root;
+                }
+                
+                int childBalanceFactor = root.leftChild.GetBalanceFactor();
+
+                if (childBalanceFactor >= 0)
+                {
+                    return RotateRight(root);
+                }
+                else
+                {
+                    root.leftChild = RotateLeft(root.leftChild);
+                    return RotateRight(root);
+                }
+            }
+
+            return root;
+        }
+
+        private TreeNode? Add(TreeNode? root, T element)
+        {
+            if (root == null) return new TreeNode(element);
+
+            int compareResult = element.CompareTo(root.element);
+
+            if (compareResult > 0)
+            {
+                root.rightChild = Add(root.rightChild, element);
+            }
+            else if (compareResult < 0)
+            {
+                root.leftChild = Add(root.leftChild, element);
+            }
+            else
+            {
+                return root;
+            }
+            root.UpdateHeight();
+            root = Rotate(root);
+            return root;
+        }
+
+        public void Add(T element)
+        {
+            root = Add(root, element);
         }
 
         private TreeNode? Remove(TreeNode? node, T element)
@@ -109,6 +189,9 @@
                     return null;
                 }
             }
+            
+            node.UpdateHeight();
+            node = Rotate(node);
 
             return node;
         }
@@ -116,6 +199,31 @@
         public void Remove(T element)
         {
             root = Remove(root, element);
+        }
+
+        public bool Contains(T element)
+        {
+            TreeNode? node = root;
+
+            while (node != null)
+            {
+                int compareResult = element.CompareTo(node.element);
+
+                if (compareResult == 0)
+                {
+                    return true;
+                }
+                else if (compareResult > 0)
+                {
+                    node = node.rightChild;
+                }
+                else
+                {
+                    node = node.leftChild;
+                }
+            }
+
+            return false;
         }
 
 #if DEBUG
@@ -146,30 +254,5 @@
             }
         }
 #endif
-
-        public bool Contains(T element)
-        {
-            TreeNode? node = root;
-
-            while (node != null)
-            {
-                int compareResult = element.CompareTo(node.element);
-
-                if (compareResult == 0)
-                {
-                    return true;
-                }
-                else if (compareResult > 0)
-                {
-                    node = node.rightChild;
-                }
-                else
-                {
-                    node = node.leftChild;
-                }
-            }
-
-            return false;
-        }
     }
 }
