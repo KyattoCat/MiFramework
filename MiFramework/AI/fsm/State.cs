f4144f549e3e6a510858bb5a2317c4b710250ff5
华锦 杨 <1196855439@qq.com>
2023-06-19 11:10:10 +0800

    FSM state

---
+++ MiFramework/AI/fsm/State.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiFramework.AI.FSM
+{
+    public abstract class State
+    {
+        public abstract void OnEnter();
+        public abstract void OnExit();
+        public abstract void OnUpdate();
+    }
+}
